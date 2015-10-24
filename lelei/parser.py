7bc63a405e278cf5d1b7d7dac0df938dfd7b7583
alfateam123 <alfateam123@gmail.com>
2015-10-24 18:00:12 +0200

    Improve field parsing: fixed-size fields may not have `bits` attribute
    Some fields whose type imposes a fixed size may not find
    useful a `bits` attribute.
    For the sake of explaining, you cannot define a "bitfield float":
    not only it makes no sense, but it's not possible to define it in
    a C struct or in WSGD format.

--- lelei/parser.py
+++ lelei/parser.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 import re
-from sizes import SIZE_CHECKERS
+from .sizes import SIZE_CHECKERS
 
 def _getroot(str_):
     return ET.fromstring(str_)	
@@ -26,7 +26,13 @@ def parse_field(field_doc):
     field_ast = dict()
     field_ast["name"] = field_doc.text
     field_ast["type"] = field_doc.attrib["type"]
-    field_ast["bits"] = bitsForStructure(field_doc.attrib["type"], int(field_doc.attrib["bits"]))
+    #some fields (e.g. float32) have a fixed size, so it's useless
+    # to set `bits=0` while defining such fields.
+    try:
+        #obviously, the KeyError is related to `bits` and not to `type`.
+        field_ast["bits"] = bitsForStructure(field_doc.attrib["type"], int(field_doc.attrib["bits"]))
+    except KeyError:
+        field_ast["bits"] = bitsForStructure(field_doc.attrib["type"], 0)
     return field_ast
 
 def build_ast(doc):
