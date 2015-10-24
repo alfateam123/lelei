c504cb9bfc1ca4b7759a99a7d0d41a5930d83c2f
alfateam123 <alfateam123@gmail.com>
2015-10-24 21:59:03 +0200

    Add support for boolX types
    WSGD uses `bool1`, `bool8`, `bool16` and `bool32`

--- tests/test_basic_types.py
+++ tests/test_basic_types.py
@@ -22,15 +22,15 @@ class TestSpareType(unittest.TestCase):
 class TestFloat(unittest.TestCase):
     
     def test_float32(self):
-        spare_desc = ET.fromstring('<field type="float32">woot</field>')
-        parsed_field = structureparser.parse_field(spare_desc)
+        float_desc = ET.fromstring('<field type="float32">woot</field>')
+        parsed_field = structureparser.parse_field(float_desc)
         self.assertEqual(parsed_field["name"], "woot")
         self.assertEqual(parsed_field["type"], "float32")
         self.assertEqual(parsed_field["bits"], 32)
 
     def test_float64(self):
-        spare_desc = ET.fromstring('<field type="float64">woot</field>')
-        parsed_field = structureparser.parse_field(spare_desc)
+        float_desc = ET.fromstring('<field type="float64">woot</field>')
+        parsed_field = structureparser.parse_field(float_desc)
         self.assertEqual(parsed_field["name"], "woot")
         self.assertEqual(parsed_field["type"], "float64")
         self.assertEqual(parsed_field["bits"], 64)
@@ -39,13 +39,39 @@ class TestPaddingBits(unittest.TestCase):
 
     def test_paddingBits(self):
         padding_desc = ET.fromstring('<field type="padding">dis_not_pudding</field>')
-        parsed_field = structureparser.parse_field(spare_desc)
+        parsed_field = structureparser.parse_field(padding_desc)
         self.assertEqual(parsed_field["name"], "dis_not_pudding")
         self.assertEqual(parsed_field["type"], "padding")
         self.assertEqual(parsed_field["bits"], 0)
 
-    def test_paddingBits(self):
+    def test_paddingBits_sizeIsIgnored(self):
         padding_desc = ET.fromstring('<field type="padding" bits="15">dis_not_pudding</field>')
         parsed_field = structureparser.parse_field(padding_desc)
         #`bits` is overridden
         self.assertEqual(parsed_field["bits"], 0) 
+
+class TestBool(unittest.TestCase):
+
+    def test_bool1(self):
+        bool_desc = ET.fromstring('<field type="bool1">a_field</field>')
+        parsed_field = structureparser.parse_field(bool_desc)
+        self.assertEqual(parsed_field["bits"], 1)
+
+    def test_bool8(self):
+        bool_desc = ET.fromstring('<field type="bool8">a_field</field>')
+        parsed_field = structureparser.parse_field(bool_desc)
+        self.assertEqual(parsed_field["bits"], 8)
+
+    def test_bool16(self):
+        bool_desc = ET.fromstring('<field type="bool16">a_field</field>')
+        parsed_field = structureparser.parse_field(bool_desc)
+        self.assertEqual(parsed_field["bits"], 16)
+
+    def test_bool32(self):
+        bool_desc = ET.fromstring('<field type="bool32">a_field</field>')
+        parsed_field = structureparser.parse_field(bool_desc)
+        self.assertEqual(parsed_field["bits"], 32)
+
+    def test_bool_doesnotexist(self):
+        bool_desc = ET.fromstring('<field type="bool25">a_field</field>')
+        self.assertRaises(ValueError, lambda : structureparser.parse_field(bool_desc))
