34d580455d62f227d1aeefdf91f95d3d09463119
alfateam123 <alfateam123@gmail.com>
2015-10-25 15:13:44 +0100

    fix merge conflicts

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -9,11 +9,11 @@ from lelei import parser
 class TestFieldBuilder(unittest.TestCase):
 
     def test_int8(self):
-        parsed_doc = parser.parse_field(ET.fromstring('<field type="uint8">test</field>'))
+        parsed_doc = parser.parse_field(ET.fromstring('<field type="uint8" bits="0">test</field>'))
         res = ws.build_field(parsed_doc)
         self.assertEqual("uint8 test;", res)
 
     def test_int8_nostmt(self):
-        parsed_doc = parser.parse_field(ET.fromstring('<field type="uint8" nostmt="2">test</field>'))
+        parsed_doc = parser.parse_field(ET.fromstring('<field type="uint8"  bits="0" nostmt="2">test</field>'))
         res = ws.build_field(parsed_doc)
         self.assertEqual("uint8{ns=0} test;", res)
