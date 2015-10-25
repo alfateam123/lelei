3e838f9717a9b47d2e88e804abd05bc8d59e53ea
alfateam123 <alfateam123@gmail.com>
2015-10-26 00:36:37 +0100

    [WIP] start generation of wsgd files, trying to fix wireshark complains :V

--- lelei/builder.py
+++ lelei/builder.py
@@ -9,6 +9,19 @@ struct {{struct_name}}
 }
 """.strip()
 
+WSDG_TEMPLATE = """
+PROTONAME      {{proto.proto_name}}
+PROTOSHORTNAME {{proto.proto_shortname}}
+PROTOABBREV    {{proto.proto_shortname}}
+
+PARENT_SUBFIELD        tcp.port
+PARENT_SUBFIELD_VALUES 20127
+
+#PROTO_TYPE_DEFINITIONS
+
+include {{fname}}.fdesc ;
+""".strip()
+
 def build_field(field_ast):
     return pystache.render("{{type}} {{name}};", field_ast)
 
@@ -19,3 +32,7 @@ def build_struct(ast):
 def build_fdesc(ast):
     return build_struct(ast)
 
+def build_wsgd(ast, proto_name):
+    #proto_info = ast["protocol_info"]
+    return pystache.render(WSDG_TEMPLATE,
+                           ast, fname=proto_name)
