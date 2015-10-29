6e541d23d828b9ad4040967f9cee7def96da6c65
alfateam123 <alfateam123@gmail.com>
2015-10-29 22:34:15 +0100

    finish implementation of multiple-structures

--- lelei/builder.py
+++ lelei/builder.py
@@ -1,4 +1,5 @@
 import pystache
+import re
 
 ENUM_TEMPLATE = """
 enum{{ast.size}} {{ast.name}}
@@ -31,7 +32,7 @@ PARENT_SUBFIELD_VALUES 20127
 MSG_HEADER_TYPE   {{header.name}}
 MSG_ID_FIELD_NAME {{header.id_field_name}}
 
-MSG_MAIN_TYPE {{struct.name}}
+MSG_MAIN_TYPE T_msg_switch({{header.id_field_name}})
 
 PROTO_TYPE_DEFINITIONS
 
@@ -39,6 +40,17 @@ include {{fname}}.fdesc ;
 """.strip()
 
 FIELD_TMPL = "{{type}} {{name}};"
+
+SWITCH_TMPL = """
+switch T_msg_switch T_type_messages
+{
+  {{#values}}
+  {{.}}
+  {{/values}}
+}
+"""
+
+SWITCH_CASE_TMPL = "case T_type_messages::{{id}} : {{struct_name}};"
 
 def build_simple_field(type_, name_):
     return pystache.render(FIELD_TMPL, type=type_, name=name_)
@@ -63,14 +75,45 @@ def build_struct(struct_ast, header_type_name=None):
 
 def build_enum(enum_ast):
     #one day, I'll understand how to do the same thing using mustache only.
-    values = [build_field({"type":value_ast["name"], "name":value_ast["id"]}) for value_ast in enum_ast["values"]]
+    values = [build_simple_field(type_=value_ast["name"], name_=value_ast["id"]) for value_ast in enum_ast["values"]]
     return pystache.render(ENUM_TEMPLATE, ast=enum_ast, values=values)
 
+def build_type_messages(ast):
+    id_field_name = ast["header"]["id_field_name"]
+    #incidentally, an IndexError will be raised here.
+    #It should be done during the parsing phase, but nobody is perfect :)
+    id_field_type = filter(lambda field: field["name"]==id_field_name, ast["header"]["fields"])[0]["type"]
+    ids_list = [build_simple_field(type_=struct_ast["name"], name_=struct_ast["struct_id"])
+                  for struct_ast in ast["structures"]]
+    return pystache.render(ENUM_TEMPLATE, ast={
+                                          "size": re.findall("(\d+)$", id_field_type)[0],
+                                          "name": "T_type_messages"
+                                          }, values=ids_list)
+
+def build_type_switch(ast):
+    # return NotImplementedError(ast)
+    id_field_name = ast["header"]["id_field_name"]
+    #incidentally, an IndexError will be raised here.
+    #It should be done during the parsing phase, but nobody is perfect :)
+    id_field_type = filter(lambda field: field["name"]==id_field_name, ast["header"]["fields"])[0]["type"]
+    ids_list = [(struct_ast["name"], struct_ast["struct_id"])
+                  for struct_ast in ast["structures"]]
+    return pystache.render(SWITCH_TMPL,
+      values = [pystache.render(SWITCH_CASE_TMPL, id=id_[1], struct_name=id_[0])
+                  for id_ in ids_list])
+
+
 def build_fdesc(ast):
+    type_messages_content = build_type_messages(ast)
     header_content     = build_struct(ast["header"])
     enums_content      = "\n".join(build_enum(enum_ast) for enum_ast in ast["enums"])
     structures_content = "\n".join(build_struct(struct_ast, ast["header"]["name"]) for struct_ast in ast["structures"])
-    return "\n".join([enums_content, header_content,structures_content])
+    switch_messages    = build_type_switch(ast)
+    return "\n".join([type_messages_content,
+                      enums_content,
+                      header_content,
+                      structures_content,
+                      switch_messages])
 
 
 def build_wsgd(ast, proto_name):
